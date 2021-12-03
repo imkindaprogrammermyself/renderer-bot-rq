226309b5c0335e6e803aaab53160d8fd15284c00
notyourfather <applicatnccnt@gmail.com>
2021-12-03 17:41:25 +0800

    Formatting.

--- bot/cogs/cog_help.py
+++ bot/cogs/cog_help.py
@@ -7,22 +7,24 @@ from discord.ext.commands import Bot, Cog, Context
 from utils.helpers import check_environ_vars
 from utils.logger import LOGGER_BOT, command_logger
 from utils.strings import *
-from ..checks import check_is_valid_channel
+from .base import PermissionCheckerCog
 
-check_environ_vars(LOGGER_BOT, 'URL_PAYPAL', 'BOT_COMMAND_PREFIX')
+check_environ_vars(LOGGER_BOT, "URL_PAYPAL", "BOT_COMMAND_PREFIX")
 
-PREFIX = getenv('BOT_COMMAND_PREFIX')
-URL_PAYPAL = getenv('URL_PAYPAL')
+PREFIX = getenv("BOT_COMMAND_PREFIX")
+URL_PAYPAL = getenv("URL_PAYPAL")
 
 
-class Help(Cog):
+class Help(PermissionCheckerCog):
     def __init__(self, bot):
         self._bot: Bot = bot
         LOGGER_BOT.info(MSG_VDU671)
 
+    # async def cog_before_invoke(self, ctx: Context):
+    #     raise PermissionError
+
     @commands.group(name="help")
-    @commands.check(check_is_valid_channel)
-    @command_logger(color=0xff99cc)
+    @command_logger(color=0xFF99CC)
     async def help(self, ctx: Context):
         if ctx.invoked_subcommand:
             await ctx.message.delete()
@@ -30,12 +32,14 @@ class Help(Cog):
         try:
             try:
                 await ctx.message.delete()
-                embed = Embed(color=0x66ff33)
+                embed = Embed(color=0x66FF33)
                 embed.set_author(name=MSG_WWC273, icon_url=MSG_YSL748)
                 embed.description = MSG_HFT616
                 embed.add_field(name=MSG_FXJ230, value=MSG_FCQ421.format(*PREFIX * 2))
                 embed.add_field(name=MSG_XOI463, value=MSG_VIJ262.format(*PREFIX * 2))
-                embed.add_field(name="\u200B", value=f"{MSG_GVT802}({URL_PAYPAL})", inline=False)
+                embed.add_field(
+                    name="\u200B", value=f"{MSG_GVT802}({URL_PAYPAL})", inline=False
+                )
                 await ctx.channel.send(embed=embed)
             except Exception:
                 pass
@@ -43,25 +47,29 @@ class Help(Cog):
             pass
 
     @help.group("render")
-    @command_logger(color=0xff99cc)
+    @command_logger(color=0xFF99CC)
     async def _render(self, ctx: Context):
         cmd = f"{PREFIX}render"
-        embed = Embed(color=0x66ff33)
+        embed = Embed(color=0x66FF33)
         embed.set_author(name=MSG_WWC273, icon_url=MSG_YSL748)
         embed.description = MSG_KUQ121
         embed.add_field(name=f"`{cmd}`", value="Normal render.", inline=False)
         embed.add_field(name=f"`{cmd} logs`", value=MSG_FVV166, inline=False)
         embed.add_field(name=f"`benny`", value=MSG_AOB487, inline=False)
         embed.add_field(name=f"`doom`", value=MSG_OTV870, inline=False)
-        embed.add_field(name=f"Syntax", value=MSG_RKN680.format(*[f'{PREFIX}render'] * 6), inline=False)
+        embed.add_field(
+            name=f"Syntax",
+            value=MSG_RKN680.format(*[f"{PREFIX}render"] * 6),
+            inline=False,
+        )
         embed.set_image(url=MSG_CCT908)
         await ctx.channel.send(embed=embed)
 
     @help.group("renderzip")
-    @command_logger(color=0xff99cc)
+    @command_logger(color=0xFF99CC)
     async def _renderzip(self, ctx: Context):
         cmd = f"{PREFIX}renderzip"
-        embed = Embed(color=0x66ff33)
+        embed = Embed(color=0x66FF33)
         embed.set_author(name=MSG_WWC273, icon_url=MSG_YSL748)
         embed.description = MSG_PYD872
         embed.add_field(name=f"`{cmd}`", value=MSG_EOG769, inline=False)
