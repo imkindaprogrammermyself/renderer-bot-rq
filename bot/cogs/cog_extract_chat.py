3bc617b14af9cf00f6646f285d87118119174836
notyourfather <applicatnccnt@gmail.com>
2021-12-04 00:25:55 +0800

    Moved the status checks to the base class. Surrounded the wile loop with try/catch.

--- bot/cogs/cog_extract_chat.py
+++ bot/cogs/cog_extract_chat.py
@@ -3,7 +3,7 @@ from io import BytesIO, StringIO
 
 import discord
 from bot.checks import check_guild_can_extract
-from discord import Attachment, Embed, File, Message, TextChannel
+from discord import Attachment, File, Message, TextChannel
 from discord.errors import Forbidden
 from discord.ext import commands
 from discord.ext.commands import Context
@@ -11,21 +11,19 @@ from rq import Queue
 from rq.job import Job
 from rq.worker import Worker
 from tasks.task_extract_chat import task_extract_chat
-from utils.constants import COLOR_ENDED, COLOR_STARTED, GREEN, ORANGE, RED, YELLOW
+from utils.constants import COLOR_CHAT_ENDED, COLOR_CHAT_STARTED, GREEN, ORANGE, RED, YELLOW
 from utils.exception import (
     ReadingError,
     UnsupportedBattleTypeError,
     VersionNotFoundError,
 )
 from utils.logger import LOGGER_BOT, command_logger_render_extract, logger_extra
-from utils.redisconn import ASYNC_REDIS, REDIS
+from utils.redisconn import REDIS
 from utils.strings import *
 
 from .base import TaskCog, track_task_request
 
-queue = Queue(connection=REDIS)
-workers = Worker([queue], connection=REDIS)
-
+QUEUE = Queue(name="chat", connection=REDIS)
 
 class ExtractChat(TaskCog):
     def __init__(self, bot):
@@ -40,35 +38,18 @@ class ExtractChat(TaskCog):
         await self._worker(ctx)
 
     async def _worker(self, ctx: Context):
-        worker_count = Worker.count(connection=REDIS, queue=queue)
-        job_ttl = max(queue.count, 1) * self._queue_max_wait_time
         message: Message = ctx.message
-        cooldown = await ASYNC_REDIS.ttl(f"cooldown_{ctx.author.id}")
-        ebd = Embed(title=MSG_OIJ303, color=0xFF751A)
-        ebd.set_thumbnail(url=MSG_YSL748)
 
-        try:
-            assert worker_count != 0, f"{message.author.mention} {MSG_DQP186}"
-            assert (
-                queue.count <= self._task_queue_size
-            ), f"{message.author.mention} {MSG_QFA769}"
-            assert cooldown <= 0, f"{message.author.mention} {MSG_IIZ122} {cooldown}s"
-            assert not await ASYNC_REDIS.exists(
-                f"task_request_{ctx.author.id}"
-            ), f"{message.author.mention} {MSG_FNB379}"
-            assert message.attachments, f"{message.author.mention} {MSG_QYM865}"
-        except AssertionError as e:
-            ebd.description = str(e)
-            await ctx.channel.send(embed=ebd, delete_after=5)
-            await self._try_delete_message(message)
+        if not await self._checks(ctx, QUEUE):
             return
 
+        job_ttl = max(QUEUE.count, 1) * self._queue_max_wait_time
         attachment: Attachment = message.attachments[0]
 
         with BytesIO() as buf:
             await attachment.save(buf)
             buf.seek(0)
-            job: Job = queue.enqueue(
+            job: Job = QUEUE.enqueue(
                 task_extract_chat,
                 args=(buf.read(), ctx.author.id),
                 failure_ttl=180,
@@ -79,7 +60,7 @@ class ExtractChat(TaskCog):
         self._bot.loop.create_task(self._poll_result(ctx, job))
         await self._try_delete_message(message)
 
-    @command_logger_render_extract((COLOR_STARTED, COLOR_ENDED))
+    @command_logger_render_extract((COLOR_CHAT_STARTED, COLOR_CHAT_ENDED))
     @track_task_request
     async def _poll_result(self, ctx: Context, job: Job):
         ch: TextChannel = ctx.channel
@@ -88,88 +69,91 @@ class ExtractChat(TaskCog):
         message: Message = await ch.send(embed=embed)
         status = "Failed"
 
-        while True:
-            position = self._get_job_position(job)
-            status = job.get_status(refresh=True)
-            if status == "queued":
-                embed = self._get_embed(ctx, ORANGE, status="Queued", position=position)
-                await message.edit(embed=embed)
-            elif status == "started":
-                try:
-                    if task_status := job.get_meta(refresh=True).get("status", None):
-                        embed = self._get_embed(ctx, YELLOW, status=task_status)
-                    else:
-                        embed = self._get_embed(ctx, YELLOW, status="Running")
-                except Exception as e:
-                    LOGGER_BOT.error(e, exc_info=e)
-                await message.edit(embed=embed)
-            elif status == "finished":
-                if isinstance(job.result, Exception):
-                    if isinstance(job.result, VersionNotFoundError):
-                        err_message = MSG_ANM988
-                    elif isinstance(job.result, UnsupportedBattleTypeError):
-                        err_message = MSG_KOL445
-                    elif isinstance(job.result, ReadingError):
-                        err_message = MSG_JYQ473
-                    else:
-                        err_message = MSG_IBK358
-                    embed = self._get_embed(
-                        ctx, RED, status="Error", result=err_message
-                    )
-                    LOGGER_BOT.error(job.result, exc_info=job.result)
-                elif isinstance(job.result, tuple):
-                    data, random_str, time_taken = job.result
-
+        try:
+            while True:
+                position = self._get_job_position(job)
+                status = job.get_status(refresh=True)
+                if status == "queued":
+                    embed = self._get_embed(ctx, ORANGE, status="Queued", position=position)
+                    await message.edit(embed=embed)
+                elif status == "started":
                     try:
-                        with StringIO(data) as reader:
-                            file = File(reader, f"chat_{random_str}.txt")
-                            chat_msg: Message = await ch.send(
-                                file=file, reference=message
+                        if task_status := job.get_meta(refresh=True).get("status", None):
+                            embed = self._get_embed(ctx, YELLOW, status=task_status)
+                        else:
+                            embed = self._get_embed(ctx, YELLOW, status="Running")
+                    except Exception as e:
+                        LOGGER_BOT.error(e, exc_info=e)
+                    await message.edit(embed=embed)
+                elif status == "finished":
+                    if isinstance(job.result, Exception):
+                        if isinstance(job.result, VersionNotFoundError):
+                            err_message = MSG_ANM988
+                        elif isinstance(job.result, UnsupportedBattleTypeError):
+                            err_message = MSG_KOL445
+                        elif isinstance(job.result, ReadingError):
+                            err_message = MSG_JYQ473
+                        else:
+                            err_message = MSG_IBK358
+                        embed = self._get_embed(
+                            ctx, RED, status="Error", result=err_message
+                        )
+                        LOGGER_BOT.error(job.result, exc_info=job.result)
+                    elif isinstance(job.result, tuple):
+                        data, random_str, time_taken = job.result
+
+                        try:
+                            with StringIO(data) as reader:
+                                file = File(reader, f"chat_{random_str}.txt")
+                                chat_msg: Message = await ch.send(
+                                    file=file, reference=message
+                                )
+                                attached_file: Attachment = chat_msg.attachments[0]
+                                result_msg = MSG_LAV349.format(
+                                    attached_file.filename,
+                                    attached_file.url,
+                                    chat_msg.jump_url,
+                                )
+                                embed = self._get_embed(
+                                    ctx,
+                                    GREEN,
+                                    status="Completed",
+                                    result=result_msg,
+                                    time_taken=time_taken,
+                                )
+                                status = "Completed"
+                        except discord.InvalidArgument as e:
+                            embed = self._get_embed(
+                                ctx, RED, status="Error", result=MSG_LKN365
                             )
-                            attached_file: Attachment = chat_msg.attachments[0]
-                            result_msg = MSG_LAV349.format(
-                                attached_file.filename,
-                                attached_file.url,
-                                chat_msg.jump_url,
+                            LOGGER_BOT.error(e, exc_info=e, extra=logger_extra(ctx))
+                        except Forbidden as e:
+                            formatted_names = ", ".join(
+                                [
+                                    " ".join(perm.split("_")).capitalize()
+                                    for perm in self._required_perm
+                                ]
                             )
                             embed = self._get_embed(
                                 ctx,
-                                GREEN,
-                                status="Completed",
-                                result=result_msg,
-                                time_taken=time_taken,
+                                RED,
+                                status="Error",
+                                result=MSG_RHH207.format(formatted_names),
                             )
-                            status = "Completed"
-                    except discord.InvalidArgument as e:
-                        embed = self._get_embed(
-                            ctx, RED, status="Error", result=MSG_LKN365
-                        )
-                        LOGGER_BOT.error(e, exc_info=e, extra=logger_extra(ctx))
-                    except Forbidden as e:
-                        formatted_names = ", ".join(
-                            [
-                                " ".join(perm.split("_")).capitalize()
-                                for perm in self._required_perm
-                            ]
-                        )
-                        embed = self._get_embed(
-                            ctx,
-                            RED,
-                            status="Error",
-                            result=MSG_RHH207.format(formatted_names),
-                        )
-                        LOGGER_BOT.error(e, exc_info=e, extra=logger_extra(ctx))
-                await message.edit(embed=embed)
-                break
-            elif status == "failed":
-                embed = self._get_embed(ctx, RED, status="Failed")
-                await message.edit(embed=embed)
-                break
-            elif not status:
-                embed = self._get_embed(ctx, RED, status="Max queue time reached.")
-                await message.edit(embed=embed)
-                break
-            await asyncio.sleep(1)
+                            LOGGER_BOT.error(e, exc_info=e, extra=logger_extra(ctx))
+                    await message.edit(embed=embed)
+                    break
+                elif status == "failed":
+                    embed = self._get_embed(ctx, RED, status="Failed")
+                    await message.edit(embed=embed)
+                    break
+                elif not status:
+                    embed = self._get_embed(ctx, RED, status="Max queue time reached.")
+                    await message.edit(embed=embed)
+                    break
+                await asyncio.sleep(1)
+        except Exception as e:
+            LOGGER_BOT.error(e, exc_info=e)
 
         job.delete()
         return status
--- bot/cogs/cog_render_dual.py
+++ bot/cogs/cog_render_dual.py
@@ -2,7 +2,7 @@ import asyncio
 from io import BytesIO
 
 import discord
-from discord import TextChannel, Message, File, Attachment, Embed
+from discord import TextChannel, Message, File, Attachment
 from discord.errors import Forbidden
 from discord.ext import commands
 from discord.ext.commands import Context
@@ -25,18 +25,14 @@ from utils.exception import (
     NotEnoughReplaysError,
 )
 from utils.logger import LOGGER_BOT, logger_extra, command_logger_render_extract
-from utils.redisconn import ASYNC_REDIS, REDIS
+from utils.redisconn import REDIS
 from utils.strings import *
 from tasks.task_render_dual import task_render_dual
 from rq import Queue
 from rq.job import Job
-from rq.worker import Worker
 from .base import TaskCog, track_task_request
 
-
-queue = Queue(connection=REDIS)
-workers = Worker([queue], connection=REDIS)
-
+QUEUE = Queue(name="dual", connection=REDIS)
 
 class RenderDual(TaskCog):
     def __init__(self, bot):
@@ -50,35 +46,18 @@ class RenderDual(TaskCog):
         await self._worker(ctx)
 
     async def _worker(self, ctx: Context):
-        worker_count = Worker.count(connection=REDIS, queue=queue)
-        job_ttl = max(queue.count, 1) * self._queue_max_wait_time
         message: Message = ctx.message
-        cooldown = await ASYNC_REDIS.ttl(f"cooldown_{ctx.author.id}")
-        ebd = Embed(title=MSG_OIJ303, color=0xFF751A)
-        ebd.set_thumbnail(url=MSG_YSL748)
 
-        try:
-            assert worker_count != 0, f"{message.author.mention} {MSG_DQP186}"
-            assert (
-                queue.count <= self._task_queue_size
-            ), f"{message.author.mention} {MSG_QFA769}"
-            assert cooldown <= 0, f"{message.author.mention} {MSG_IIZ122} {cooldown}s"
-            assert not await ASYNC_REDIS.exists(
-                f"task_request_{ctx.author.id}"
-            ), f"{message.author.mention} {MSG_FNB379}"
-            assert message.attachments, f"{message.author.mention} {MSG_QYM865}"
-        except AssertionError as e:
-            ebd.description = str(e)
-            await ctx.channel.send(embed=ebd, delete_after=5)
-            await self._try_delete_message(message)
+        if not await self._checks(ctx, QUEUE):
             return
 
+        job_ttl = max(QUEUE.count, 1) * self._queue_max_wait_time
         attachment: Attachment = message.attachments[0]
 
         with BytesIO() as buf:
             await attachment.save(buf)
             buf.seek(0)
-            job: Job = queue.enqueue(
+            job: Job = QUEUE.enqueue(
                 task_render_dual,
                 args=(buf.read(), ctx.author.id),
                 failure_ttl=180,
@@ -98,100 +77,103 @@ class RenderDual(TaskCog):
         message: Message = await ch.send(embed=embed)
         status = "Failed"
 
-        while True:
-            position = self._get_job_position(job)
-            status = job.get_status(refresh=True)
-            if status == "queued":
-                embed = self._get_embed(ctx, ORANGE, status="Queued", position=position)
-                await message.edit(embed=embed)
-            elif status == "started":
-                try:
-                    if progress := job.get_meta(refresh=True).get("progress", None):
-                        progress = round(progress * 10)
-                        embed = self._get_embed(
-                            ctx, YELLOW, status="Rendering", per=progress
-                        )
-                    elif task_status := job.get_meta(refresh=True).get("status", None):
-                        embed = self._get_embed(ctx, YELLOW, status=task_status)
-                    else:
-                        embed = self._get_embed(ctx, YELLOW, status="Running")
-                except Exception as e:
-                    LOGGER_BOT.error(e, exc_info=e)
-                await message.edit(embed=embed)
-            elif status == "finished":
-                if isinstance(job.result, Exception):
-                    if isinstance(job.result, VersionNotFoundError):
-                        err_message = MSG_ANM988
-                    elif isinstance(job.result, UnsupportedBattleTypeError):
-                        err_message = MSG_KOL445
-                    elif isinstance(job.result, ReadingError):
-                        err_message = MSG_JYQ473
-                    elif isinstance(job.result, RenderingError):
-                        err_message = MSG_HIY955
-                    elif isinstance(e, ArenaIdMismatchError):
-                        err_message = MSG_TOG346
-                    elif isinstance(e, MultipleReplaysError):
-                        err_message = MSG_ATK550
-                    elif isinstance(e, NotEnoughReplaysError):
-                        err_message = MSG_MDF285
-                    elif isinstance(e, FileNotFoundError):
-                        err_message = str(e)
-                    else:
-                        err_message = MSG_IBK358
-                    embed = self._get_embed(
-                        ctx, RED, status="Error", result=err_message
-                    )
-                    LOGGER_BOT.error(job.result, exc_info=job.result)
-                elif isinstance(job.result, tuple):
-                    video_data, random_str, time_taken = job.result
+        try:
+            while True:
+                position = self._get_job_position(job)
+                status = job.get_status(refresh=True)
+                if status == "queued":
+                    embed = self._get_embed(ctx, ORANGE, status="Queued", position=position)
+                    await message.edit(embed=embed)
+                elif status == "started":
                     try:
-                        with BytesIO(video_data) as reader:
-                            file = File(reader, f"{random_str}.mp4")
-                        video_msg: Message = await ch.send(file=file, reference=message)
-                        attached_file: Attachment = video_msg.attachments[0]
-                        result_msg = MSG_LAV349.format(
-                            attached_file.filename,
-                            attached_file.url,
-                            video_msg.jump_url,
-                        )
+                        if progress := job.get_meta(refresh=True).get("progress", None):
+                            progress = round(progress * 10)
+                            embed = self._get_embed(
+                                ctx, YELLOW, status="Rendering", per=progress
+                            )
+                        elif task_status := job.get_meta(refresh=True).get("status", None):
+                            embed = self._get_embed(ctx, YELLOW, status=task_status)
+                        else:
+                            embed = self._get_embed(ctx, YELLOW, status="Running")
+                    except Exception as e:
+                        LOGGER_BOT.error(e, exc_info=e)
+                    await message.edit(embed=embed)
+                elif status == "finished":
+                    if isinstance(job.result, Exception):
+                        if isinstance(job.result, VersionNotFoundError):
+                            err_message = MSG_ANM988
+                        elif isinstance(job.result, UnsupportedBattleTypeError):
+                            err_message = MSG_KOL445
+                        elif isinstance(job.result, ReadingError):
+                            err_message = MSG_JYQ473
+                        elif isinstance(job.result, RenderingError):
+                            err_message = MSG_HIY955
+                        elif isinstance(job.result, ArenaIdMismatchError):
+                            err_message = MSG_TOG346
+                        elif isinstance(job.result, MultipleReplaysError):
+                            err_message = MSG_ATK550
+                        elif isinstance(job.result, NotEnoughReplaysError):
+                            err_message = MSG_MDF285
+                        elif isinstance(job.result, FileNotFoundError):
+                            err_message = str(job.result)
+                        else:
+                            err_message = MSG_IBK358
                         embed = self._get_embed(
-                            ctx,
-                            GREEN,
-                            status="Completed",
-                            result=result_msg,
-                            time_taken=time_taken,
+                            ctx, RED, status="Error", result=err_message
                         )
-                        status = "Completed"
-                    except discord.InvalidArgument as e:
-                        embed = self._get_embed(
-                            ctx, RED, status="Error", result=MSG_LKN365
-                        )
-                        LOGGER_BOT.error(e, exc_info=e, extra=logger_extra(ctx))
-                    except Forbidden as e:
-                        formatted_names = ", ".join(
-                            [
-                                " ".join(perm.split("_")).capitalize()
-                                for perm in self._required_perm
-                            ]
-                        )
-                        embed = self._get_embed(
-                            ctx,
-                            RED,
-                            status="Error",
-                            result=MSG_RHH207.format(formatted_names),
-                        )
-                        LOGGER_BOT.error(e, exc_info=e, extra=logger_extra(ctx))
-                await message.edit(embed=embed)
-                break
-            elif status == "failed":
-                embed = self._get_embed(ctx, RED, status="Failed")
-                await message.edit(embed=embed)
-                break
-            elif not status:
-                embed = self._get_embed(ctx, RED, status="Max queue time reached.")
-                await message.edit(embed=embed)
-                break
-            await asyncio.sleep(1)
+                        LOGGER_BOT.error(job.result, exc_info=job.result)
+                    elif isinstance(job.result, tuple):
+                        video_data, random_str, time_taken = job.result
+                        try:
+                            with BytesIO(video_data) as reader:
+                                file = File(reader, f"{random_str}.mp4")
+                            video_msg: Message = await ch.send(file=file, reference=message)
+                            attached_file: Attachment = video_msg.attachments[0]
+                            result_msg = MSG_LAV349.format(
+                                attached_file.filename,
+                                attached_file.url,
+                                video_msg.jump_url,
+                            )
+                            embed = self._get_embed(
+                                ctx,
+                                GREEN,
+                                status="Completed",
+                                result=result_msg,
+                                time_taken=time_taken,
+                            )
+                            status = "Completed"
+                        except discord.InvalidArgument as e:
+                            embed = self._get_embed(
+                                ctx, RED, status="Error", result=MSG_LKN365
+                            )
+                            LOGGER_BOT.error(e, exc_info=e, extra=logger_extra(ctx))
+                        except Forbidden as e:
+                            formatted_names = ", ".join(
+                                [
+                                    " ".join(perm.split("_")).capitalize()
+                                    for perm in self._required_perm
+                                ]
+                            )
+                            embed = self._get_embed(
+                                ctx,
+                                RED,
+                                status="Error",
+                                result=MSG_RHH207.format(formatted_names),
+                            )
+                            LOGGER_BOT.error(e, exc_info=e, extra=logger_extra(ctx))
+                    await message.edit(embed=embed)
+                    break
+                elif status == "failed":
+                    embed = self._get_embed(ctx, RED, status="Failed")
+                    await message.edit(embed=embed)
+                    break
+                elif not status:
+                    embed = self._get_embed(ctx, RED, status="Max queue time reached.")
+                    await message.edit(embed=embed)
+                    break
+                await asyncio.sleep(1)
+        except Exception as e:
+            LOGGER_BOT.error(e, exc_info=e)
 
         job.delete()
         return status
